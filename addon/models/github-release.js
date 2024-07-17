efd09c4db354f0f34d2573a6c73e1557fd2490ff
Yehuda Katz <wycats@gmail.com>
2024-07-17 11:00:59 -0700

    Remove deprecated features

--- addon/models/github-release.js
+++ addon/models/github-release.js
@@ -1,29 +1,33 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
-import { deprecate } from '@ember/application/deprecations';
-import { computed } from '@ember/object';
+import Model from "ember-data/model";
+import attr from "ember-data/attr";
+import { belongsTo } from "ember-data/relationships";
+import { deprecate } from "@ember/debug";
+import { computed } from "@ember/object";
 
 export default Model.extend({
-  name: attr('string'),
-  url: attr('string'),
-  htmlUrl: attr('string'),
-  assetsUrl: attr('string'),
-  uploadUrl: attr('string'),
-  tarballUrl: attr('string'),
-  zipballUrl: attr('string'),
-  tagName: attr('string'),
-  targetCommitish: attr('string'),
-  body: attr('string'),
-  draft: attr('boolean'),
-  prerelease: attr('boolean'),
-  createdAt: attr('date'),
-  publishedAt: attr('date'),
+  name: attr("string"),
+  url: attr("string"),
+  htmlUrl: attr("string"),
+  assetsUrl: attr("string"),
+  uploadUrl: attr("string"),
+  tarballUrl: attr("string"),
+  zipballUrl: attr("string"),
+  tagName: attr("string"),
+  targetCommitish: attr("string"),
+  body: attr("string"),
+  draft: attr("boolean"),
+  prerelease: attr("boolean"),
+  createdAt: attr("date"),
+  publishedAt: attr("date"),
 
-  author: belongsTo('github-user', { inverse: null }),
-  user: computed('author', function() {
-    deprecate('The user property on the github-release model has been deprecated.  Please use the author property.', false, { id: 'ember-data-github.deprecated-model-props', until: '1.0.0' });
-    return this.get('author');
+  author: belongsTo("github-user", { inverse: null }),
+  user: computed("author", function () {
+    deprecate(
+      "The user property on the github-release model has been deprecated.  Please use the author property.",
+      false,
+      { id: "ember-data-github.deprecated-model-props", until: "1.0.0" }
+    );
+    return this.get("author");
   }),
-  repository: belongsTo('github-repository')
+  repository: belongsTo("github-repository"),
 });
--- addon/models/github-user.js
+++ addon/models/github-user.js
@@ -1,49 +1,53 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
-import { deprecate } from '@ember/application/deprecations';
-import { computed } from '@ember/object';
+import Model from "ember-data/model";
+import attr from "ember-data/attr";
+import { hasMany } from "ember-data/relationships";
+import { deprecate } from "@ember/debug";
+import { computed } from "@ember/object";
 
 export default Model.extend({
-  avatarUrl: attr('string'),
-  bio: attr('string'),
-  blog: attr('string'),
-  company: attr('string'),
-  email: attr('string'),
-  gravatarId: attr('string'),
-  location: attr('string'),
-  login: attr('string'),
-  name: attr('string'),
-  type: attr('string'),
+  avatarUrl: attr("string"),
+  bio: attr("string"),
+  blog: attr("string"),
+  company: attr("string"),
+  email: attr("string"),
+  gravatarId: attr("string"),
+  location: attr("string"),
+  login: attr("string"),
+  name: attr("string"),
+  type: attr("string"),
 
-  followers: attr('number'),
-  following: attr('number'),
-  publicGists: attr('number'),
-  publicRepos: attr('number'),
+  followers: attr("number"),
+  following: attr("number"),
+  publicGists: attr("number"),
+  publicRepos: attr("number"),
 
-  createdAt: attr('date'),
-  updatedAt: attr('date'),
+  createdAt: attr("date"),
+  updatedAt: attr("date"),
 
-  hireable: attr('boolean'),
-  siteAdmin: attr('boolean'),
+  hireable: attr("boolean"),
+  siteAdmin: attr("boolean"),
 
   // Urls
-  eventsUrl: attr('string'),
-  followersUrl: attr('string'),
-  followingUrl: attr('string'),
-  gistsUrl: attr('string'),
-  htmlUrl: attr('string'),
-  organizationsUrl: attr('string'),
-  receivedEventsUrl: attr('string'),
-  reposUrl: attr('string'),
-  starredUrl: attr('string'),
-  subscriptionsUrl: attr('string'),
-  url: attr('string'),
+  eventsUrl: attr("string"),
+  followersUrl: attr("string"),
+  followingUrl: attr("string"),
+  gistsUrl: attr("string"),
+  htmlUrl: attr("string"),
+  organizationsUrl: attr("string"),
+  receivedEventsUrl: attr("string"),
+  reposUrl: attr("string"),
+  starredUrl: attr("string"),
+  subscriptionsUrl: attr("string"),
+  url: attr("string"),
 
   // Embedded Objects
-  repositories: hasMany('github-repository'),
-  githubRepositories: computed('repositories.[]', function() {
-    deprecate('The githubRepositories property on the github-user model has been deprecated.  Please use the repositories property.', false, { id: 'ember-data-github.deprecated-model-props', until: '1.0.0' });
-    return this.get('repositories');
+  repositories: hasMany("github-repository"),
+  githubRepositories: computed("repositories.[]", function () {
+    deprecate(
+      "The githubRepositories property on the github-user model has been deprecated.  Please use the repositories property.",
+      false,
+      { id: "ember-data-github.deprecated-model-props", until: "1.0.0" }
+    );
+    return this.get("repositories");
   }),
 });
